fe22be67ad1141a31799c1df86dd803bb02f54bf
96sam <96sam@DESKTOP-U2TUVL3.mynetworksettings.com>
2023-04-07 20:48:15 -0400

    commit

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,9 +2,8 @@ package day2;
 
 public class Day2Demo {
 
-	public static void main(String [] args) {
-		
-		System.out.println(" Day 2 Git");
-		
+
+	public void multiply() {
+		System.out.println("code...");
 	}
 }
